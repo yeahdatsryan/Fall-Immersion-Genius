5dff2ccafe59e07ed91b5ee91615462fac5481f5
yeahdatsryan <ryan.price@hgs.hiddengeniusproject.org>
2024-10-22 17:55:16 -0400

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,13 @@
 function darkMode() {
-// Your code here.
+const variable = document.body;
+
+  function darkMode(){
+
+    if(doument.style.backgrondcole == 'white'
+document.style.color = 'black'
+document.style.color= 'white'
+}
+else{
+  document.style.backgroundcolor = 'white';
+  document.style.color = 'black';
 }
