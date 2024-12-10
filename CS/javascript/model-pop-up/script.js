38bf5aedc13e3ac7c55ddfc306927ddc21963252
yeahdatsryan <ryan.price@hgs.hiddengeniusproject.org>
2024-12-10 18:05:51 -0500

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,15 @@
-function modelPopUp() {
-    // Your code here.
-}   
+document.addEventListener ("DOMContentLoaded", () => {
+     const form = document.getElementById("myForm");
+     form.addEventListener ('submit', function (event) {
+          event.preventDefault();
+          uname = document.getElementById("name");
+          email = document.getElementById("email");
+          message = document.getElementById("message");
+          if (uname && email && message) {
+               alert(`Form submitted successfully!\nName: ${uname.value}\nEmail: ${email.value}\nMessage: ${message.value}`);
+          }
+          else{
+                alert('Fill all the fields!');
+          }
+     });
+});
