ab137d05c4486085d0be99a027eaaf26cbb7e09d
yeahdatsryan <ryan.price@hgs.hiddengeniusproject.org>
2024-12-14 18:49:38 -0500

    Update script.js

--- CS/bootstrap/Clothing-Store-App/script.js
+++ CS/bootstrap/Clothing-Store-App/script.js
@@ -1,5 +1,1 @@
-// Initialize tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+
