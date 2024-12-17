95e2ffd0182e1b76c0b5ce44703a9e0c969f4b94
yeahdatsryan <ryan.price@hgs.hiddengeniusproject.org>
2024-12-17 17:12:42 -0500

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,5 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById("MyButton").addEventListener("'click', function (){
+  image = document.createElement('img');
+  image.src = "https://i.scdn.co/image/ab67616d0000b273090b1f9557a6d23c0817bd88";
+  document.body.appendChild(image);
+});
